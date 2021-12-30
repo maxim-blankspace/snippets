902123e192a50c72b02a21a102a6e981e1fdfd7d
maxim-blankspace <maxim@blankspaceinc.ca>
2021-12-30 19:36:09 +0300

    animated-cup

---
+++ animated-cup/animated-cup.tsx
@@ -0,0 +1,68 @@
+import { Box } from '@components/index'
+import { useEffect, useRef } from 'react'
+import { vs } from 'react-native-size-matters/extend'
+import { Image, StyleSheet } from 'react-native'
+import Animated, { useSharedValue, useAnimatedStyle, Easing, withTiming, withDelay } from 'react-native-reanimated'
+import LottieView from 'lottie-react-native'
+
+type CupProps = {
+  progress: number // 0..100
+}
+
+const AnimatedCup = ({ progress }: CupProps) => {
+  const lottieRef = useRef()
+  const cupTransY = useSharedValue(0)
+
+  const animatedCup = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: cupTransY.value }],
+    }
+  })
+
+  useEffect(() => {
+    lottieRef?.current.play(36.8, 36.9)
+    lottieRef?.current.play(36.9, 55)
+    cupTransY.value = withDelay(
+      600,
+      withTiming((vs(-52) * progress) / 100, {
+        duration: 900,
+        easing: Easing.out(Easing.cubic),
+      }),
+    )
+  }, [])
+
+  return (
+    <Box height={vs(125)} bg="white" style={styles.overflowBox}>
+      <Box width={vs(100)} style={styles.overflowBox}>
+        <LottieView
+          ref={lottieRef}
+          loop={false}
+          speed={0.7}
+          source={require('@assets/animations/droplet2.json')}
+          style={styles.droplet}
+        />
+      </Box>
+      <Animated.View style={[animatedCup, styles.liquidWrapper]}>
+        <Image source={require('@assets/images/liquid4.png')} style={styles.liquid} />
+      </Animated.View>
+      <Box position="absolute">
+        <Image source={require('@assets/images/cup3.png')} style={styles.cup} />
+      </Box>
+    </Box>
+  )
+}
+
+const styles = StyleSheet.create({
+  overflowBox: { overflow: 'hidden', alignItems: 'center' },
+  liquid: {
+    width: vs(100),
+    height: vs(52),
+    alignItems: 'center',
+    transform: [{ translateX: vs(4) }],
+  },
+  liquidWrapper: { position: 'absolute', bottom: vs(-30) },
+  cup: { width: vs(147), height: vs(125) },
+  droplet: { height: vs(300), top: vs(-34.3) },
+})
+
+export default AnimatedCup
